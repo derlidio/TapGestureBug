5e32f2e1d6b6fa69f7b97fac19258a2b22cae66d
Derlidio Siqueira <derlidio@gmail.com>
2023-11-23 18:39:37 -0300

    Initial Commit

---
+++ TapGestureBug/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TapGestureBug;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ TapGestureBug/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows.Input;
+
+namespace TapGestureBug;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+    public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+        Tapped();
+	}
+
+    void Tapped(object sender, TappedEventArgs e)
+    {
+        Tapped();
+    }
+
+    private void Tapped()
+    {
+        count++;
+
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time";
+        else
+            CounterBtn.Text = $"Clicked {count} times";
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+}
